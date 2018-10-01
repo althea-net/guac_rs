81144b809c621517e9ea2015c1dca5289ae64702
Michał Papierski <michal@papierski.net>
2018-10-01 15:02:17 +0200

    Temporarily comment out errors.

--- guac_actix/src/network_endpoints.rs
+++ guac_actix/src/network_endpoints.rs
@@ -22,13 +22,14 @@ pub fn init_server(port: u16) {
         App::new()
             .resource("/update", |r| {
                 r.method(Method::POST).with_async(update_endpoint)
-            }).resource("/propose", |r| {
-                r.method(Method::POST).with_async(propose_channel_endpoint)
-            }).resource("/channel_created", |r| {
-                r.method(Method::POST).with_async(channel_created_endpoint)
-            }).resource("/channel_joined", |r| {
-                r.method(Method::POST).with_async(channel_joined_endpoint)
             })
+            // .resource("/propose", |r| {
+            //     r.method(Method::POST).with_async(propose_channel_endpoint)
+            // }).resource("/channel_created", |r| {
+            //     r.method(Method::POST).with_async(channel_created_endpoint)
+            // }).resource("/channel_joined", |r| {
+            //     r.method(Method::POST).with_async(channel_joined_endpoint)
+            // })
     }).bind(&format!("[::0]:{}", port))
     .unwrap()
     .start();
