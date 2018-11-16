9ae5ad97819262bf768b1dd05a3cd1882db33133
Michał Papierski <michal@papierski.net>
2018-11-16 21:35:22 +0100

    Update CI for guac contract.
    While waiting for new clarity build with updates API this change will
    make it easier to track build failures related to contract ABI change.

--- scripts/chain-prep.sh
+++ scripts/chain-prep.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 killall node 2>/dev/null || true
 ganache-cli -u 0 -u 1 -u 2 -m 'cook mango twist then skin sort option civil have still rather guilt' &
-git clone https://github.com/althea-mesh/simple-bidirectional-erc20-channel contract
+git clone https://github.com/althea-mesh/guac contract
 pushd contract 
 npm install .
 truffle compile
 truffle migrate --verbose
-export CHANNEL_ADDRESS=`jq -r '.networks| to_entries | sort_by(.key) | last.value.address' build/contracts/ChannelManager.json`
-export TOKEN_ADDRESS=`jq -r '.networks| to_entries | sort_by(.key) | last.value.address' build/contracts/SimpleToken.json`
+export CHANNEL_ADDRESS=`jq -r '.networks| to_entries | sort_by(.key) | last.value.address' build/contracts/PaymentChannels.json`
 popd
