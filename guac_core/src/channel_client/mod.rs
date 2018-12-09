0a1dcb7b49a25a057c942af7f7c8dac410e8f835
Jehan <jehan.tremback@gmail.com>
2018-12-08 19:31:19 -0800

    Delete dead files

--- guac_core/src/channel_client/mod.rs
+++ guac_core/src/channel_client/mod.rs
@@ -1,7 +1,5 @@
-// pub mod channel_manager;
-pub mod channel_manager_new;
+pub mod channel_manager;
 pub mod combined_state;
 pub mod types;
 
-// pub use self::channel_manager::{ChannelManager, ChannelManagerAction};
 pub use self::types::Channel;
