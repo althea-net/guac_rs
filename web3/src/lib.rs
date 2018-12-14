935bde99aaffdb7a19540192c7d2cf51232a3f9a
Jehan <jehan.tremback@gmail.com>
2018-12-14 13:53:54 -0800

    moved web3 into own crate

---
+++ web3/src/lib.rs
@@ -0,0 +1,16 @@
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+extern crate clarity;
+extern crate futures;
+extern crate num256;
+#[macro_use]
+extern crate failure;
+extern crate actix_web;
+extern crate futures_timer;
+extern crate tokio;
+
+pub mod client;
+pub mod jsonrpc;
+pub mod types;
